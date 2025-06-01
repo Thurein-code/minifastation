7e2f450601c146e6b3813f97e01419daa6b60bd7
Thurein Kyaw <thureinkyaw9.08@gmail.com>
2025-06-01 19:19:39 +0630

    Updated

--- java.js
+++ java.js
@@ -1,7 +1,11 @@
-const btn = document.getElementById("check");
+const checkB = document.getElementById("check");
 const number = document.getElementById('number');
+const goalList = document.getElementById('goalList');
+const addListB = 
 
-btn.addEventListener('click', ()=>{
+checkB.addEventListener('click', ()=>{
   const random = Math.floor( Math.random() * 10000000) +1;
   number.textContent = random;
 })
+
+goalList.addEventListener
