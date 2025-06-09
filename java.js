8408a3a7a4d24a62bd5342622489643f79e459ef
Thurein Kyaw <thureinkyaw9.08@gmail.com>
2025-06-09 13:43:22 +0630

    Did some changes

--- java.js
+++ java.js
@@ -1,11 +1,21 @@
 const checkB = document.getElementById("check");
 const number = document.getElementById('number');
 const goalList = document.getElementById('goalList');
-const addListB = 
+const addListB = document.getElementById('addList');
+const UList = document.getElementById('list')
 
 checkB.addEventListener('click', ()=>{
   const random = Math.floor( Math.random() * 10000000) +1;
   number.textContent = random;
 })
 
-goalList.addEventListener
+addListB.addEventListener('click' ,() => {
+  const task = goalList.value.trim();
+
+  if (task === '' ) return;
+  
+  const newList = document.createElement('li');
+  newList.textContent = task;
+  UList.appendChild(newList);
+  goalList.value = " ";
+})
